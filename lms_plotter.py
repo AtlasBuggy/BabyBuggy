63b92a5c4549da777b141ada6548a46eba62de57
Woz4tetra <woz4tetra@gmail.com>
2017-09-30 12:08:10 -0400

    first rolls

--- lms_plotter.py
+++ lms_plotter.py
@@ -13,15 +13,15 @@ def key_press_fn(event):
 
 robot = Robot(write=False, log_level=10)
 
-map_size_pixels = 1600
-map_size_meters = 50
+map_size_pixels = 3200
+map_size_meters = 500
 
-file_name = "2017_Sep_29/19;23;06.log.xz"
+file_name = "2017_Sep_30/05;45;57.log.xz"
 
 sicklms = LmsSimulator()
-slam = Slam(map_size_pixels, map_size_meters, write_image=True, enabled=True)
+slam = Slam(map_size_pixels, map_size_meters, write_image=True, enabled=True, perform_slam=False, plot_slam=False)
 plotter = LivePlotter(2, matplotlib_events=dict(key_press_event=key_press_fn))
-log_parser = LogParser(file_name, "logs")
+log_parser = LogParser(file_name, "logs", update_rate=0.005)
 
 slam.subscribe(Feed(slam.lms_tag, sicklms))
 slam.subscribe(Subscription(slam.plotter_tag, plotter))
--- runner.py
+++ runner.py
@@ -13,16 +13,16 @@ def key_press_fn(event):
 
 robot = Robot(write=True)
 
-recorder = VideoRecorder(enabled=True, directory="videos")
+recorder = VideoRecorder(enabled=True, directory="videos/2017_Sep_29")
 camera = CameraStream(capture_number=1, width=800, height=500, enabled=True)
 viewer = CameraViewer(enable_trackbar=False, enabled=True)
 
-map_size_pixels = 1600
-map_size_meters = 50
+map_size_pixels = 3200
+map_size_meters = 500
 
 sicklms = LMS200("/dev/serial/by-id/usb-Prolific_Technology_Inc._USB-Serial_Controller-if00-port0",
                  enabled=True)
-slam = Slam(map_size_pixels, map_size_meters, write_image=False, enabled=True, perform_slam=False, plot_slam=False)
+slam = Slam(map_size_pixels, map_size_meters, write_image=True, enabled=True, perform_slam=True, plot_slam=False)
 plotter = LivePlotter(2, matplotlib_events=dict(key_press_event=key_press_fn), enabled=True)
 
 recorder.subscribe(Feed(recorder.capture_tag, camera))
