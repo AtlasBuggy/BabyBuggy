a7a34ef28e8cca4dfdf0baa4da98420c62db8564
Woz4tetra <woz4tetra@gmail.com>
2017-10-06 17:51:48 -0400

    Added the GPS

--- babybuggy/babybuggyserial.py
+++ babybuggy/babybuggyserial.py
@@ -1,13 +1,19 @@
 from atlasbuggy.microcontrollers import SerialStream
 from .imu import IMU
+from .gps import GPS
 
 
 class BabyBuggySerial(SerialStream):
     def __init__(self):
         self.imu = IMU()
-        super(BabyBuggySerial, self).__init__(self.imu)
+        self.gps = GPS()
+        super(BabyBuggySerial, self).__init__(self.imu, self.gps)
 
         self.link_callback(self.imu, self.received_imu)
+        self.link_callback(self.gps, self.received_gps)
 
     def received_imu(self, timestamp, packet):
         print("%0.4f, %0.4f, %0.4f" % self.imu.euler.get_tuple())
+
+    def received_gps(self, timestamp, packet):
+        print(self.gps)
