0217732fa6798416a7383f17ac70a858fb47448e
Woz4tetra <woz4tetra@gmail.com>
2017-10-27 20:07:14 -0400

    It's a bit jank but it works

--- runner.py
+++ runner.py
@@ -4,15 +4,15 @@ from babybuggy.bno055 import BNO055
 
 from lms200 import Slam, LMS200
 from atlasbuggy import Orchestrator, run
-from atlasbuggy.opencv import OpenCVCamera, OpenCVRecorder
+from atlasbuggy.opencv import OpenCVCamera, OpenCVRecorder, OpenCVViewer
 
 map_size_pixels = 1600
 map_size_meters = 50
 
-enable_arduinos = False
+enable_arduinos = True
 enable_camera = True
 enable_video_recording = True
-enable_lidar = False
+enable_lidar = True
 
 class BabyBuggy(Orchestrator):
     def __init__(self, event_loop):
@@ -22,22 +22,22 @@ class BabyBuggy(Orchestrator):
         video_file_name = self.file_name[:-3] + "avi"
         video_directory = "videos/" + os.path.join(*self.directory.split(os.sep)[1:])  # remove "log" part of directory
 
-        # bno055 = BNO055(enabled=enable_arduinos)
-        # adafruit_gps = AdafruitGPS(enabled=enable_arduinos)
-        # sicklms = LMS200(
-        #     "/dev/serial/by-id/usb-Prolific_Technology_Inc._USB-Serial_Controller-if00-port0",
-        #     # "/dev/cu.usbserial"
-        #     enabled=enable_lidar
-        # )
-        # slam = Slam(map_size_pixels, map_size_meters, write_image=True, enabled=enable_lidar)
-        # OpenCVCamera.ignore_capture_numbers(0)
+        bno055 = BNO055(enabled=enable_arduinos)
+        adafruit_gps = AdafruitGPS(enabled=enable_arduinos)
+        sicklms = LMS200(
+            "/dev/serial/by-id/usb-Prolific_Technology_Inc._USB-Serial_Controller-if00-port0",
+            # "/dev/cu.usbserial"
+            enabled=enable_lidar
+        )
+        slam = Slam(map_size_pixels, map_size_meters, write_image=True, enabled=enable_lidar)
+        OpenCVCamera.ignore_capture_numbers(0)
         camera = OpenCVCamera(capture_number=1, enabled=enable_camera)
         recorder = OpenCVRecorder(video_file_name, video_directory, enabled=enable_video_recording)
 
-        # self.add_nodes(bno055, adafruit_gps, sicklms, camera, recorder, slam)
-        self.add_nodes(camera, recorder)
+        # FIXME: order matters. Arduinos need to go first before LMS
+        self.add_nodes(bno055, adafruit_gps, sicklms, camera, recorder, slam)
 
-        # self.subscribe(sicklms, slam, slam.lms_tag)
+        self.subscribe(sicklms, slam, slam.lms_tag)
         self.subscribe(camera, recorder, recorder.capture_tag)
 
 
