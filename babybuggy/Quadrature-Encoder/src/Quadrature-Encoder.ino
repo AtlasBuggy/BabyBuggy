e700a7826e60a015a6875f2417075ed5a0a72cba
Woz4tetra <woz4tetra@gmail.com>
2017-11-08 13:40:41 -0500

    Added quadrature encoders

---
+++ babybuggy/Quadrature-Encoder/src/Quadrature-Encoder.ino
@@ -0,0 +1,38 @@
+#include <Encoder.h>
+#include <Atlasbuggy.h>
+
+Atlasbuggy robot("Quadrature-Encoder");
+Encoder encoder1(3, 4);
+
+long oldPosition = -1;
+unsigned long prev_time = millis();
+
+void setup() {
+    robot.begin();
+}
+
+void loop() {
+    while (robot.available()) {
+        robot.readSerial();
+        int status = robot.readSerial();
+        if (status == 2) {  // start event
+            oldPosition = -1;
+            encoder1.write(0);
+        }
+        // else if (status == 1) {  // stop event
+        //
+        // }
+        // else if (status == 0) {  // user command
+        //
+        // }
+    }
+    if (!robot.isPaused()) {
+        encoder1.read();
+        long newPosition = encoder1.read();
+        if (newPosition != oldPosition) {
+            oldPosition = newPosition;
+            Serial.print(newPosition);
+            Serial.print('\n');
+        }
+    }
+}
